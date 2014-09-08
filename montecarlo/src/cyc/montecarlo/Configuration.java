1dd1ecf8e01ae36a979af1eb1b4d9dc6dac7fedb
Alfonso Kim <alfonsokim84@gmail.com>
2014-09-08 18:17:07 -0500

    commit inicial

---
+++ montecarlo/src/cyc/montecarlo/Configuration.java
@@ -0,0 +1,16 @@
+package cyc.montecarlo;
+
+/**
+ * 
+ * @author Alfonso Kim
+ */
+public class Configuration {
+	
+	/**
+	 * 
+	 */
+	public Configuration() { }
+
+	
+	
+}
---
+++ montecarlo/src/cyc/montecarlo/Montecarlo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package cyc.montecarlo;
+
+/**
+ * @author Alfonso Kim
+ */
+public class Montecarlo {
+
+	/**
+	 * 
+	 */
+	public Montecarlo() { }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
